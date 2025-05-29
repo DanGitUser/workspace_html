6775b3d79b04d591b39279143876e1a06b13c54f
DanGitUser <imtaehoonkim@gmail.com>
2025-05-29 18:20:54 +0900

    frontend day 10

---
+++ website 2/usersList.js
@@ -0,0 +1,8 @@
+const users = [
+    { name: "철수", age: 17 },
+    { name: "영희", age: 22 },
+    { name: "민수", age: 35 },
+    { name: "지우", age: 15 },
+];
+// 20세 이상인 유저만 <li>로 만들어 #userList에 출력하세요.
+// 출력 예: <li>영희 (22세)</li>, <li>민수 (35세)</li>
